36fd09cee68a942a66647c62e725d1aae4577330
Silvia Jiang <104666757+byjtw@users.noreply.github.com>
2022-11-29 09:46:52 +0800

    H1U4 added

---
+++ H1U4/data/01.js
@@ -0,0 +1,87 @@
+﻿//=======================================================
+// HTML5 Spelling 題庫設定檔: 使用自定的 MP3 語音檔
+//=======================================================
+//
+//---------------------------------------------------
+//【TTS 語音設定】
+//---------------------------------------------------
+//
+//是否使用 TTS 的語音(使用: true, 不使用: false)
+tts_enabled = true;
+
+//
+//----------------
+//Google TTS 文字轉語音的設定
+//【注意】不能和 iSpeech TTS 同時使用
+//----------------
+//
+//如果要使用 Google TTS，就拿掉下面這[4行]最左邊的註解(兩個斜線)
+tts_language = 'en';  // en : 英語,   zh_tw : 中文
+tts_speed = 0.3;  //語音的速度 0 ~ 1 (可用小數)
+tts_base_url = 'https://translate.google.com/translate_tts?ie=UTF-8&tl='+tts_language+'&client=tw-ob&ttsspeed='+tts_speed+'&q=';
+tts_cut_last_seconds = 0; //裁掉最後面的聲音長度(秒為單位)
+
+//
+//----------------
+//iSpeech TTS 文字轉語音的設定
+//【注意】不能和 Google TTS 同時使用
+//----------------
+//原始網站參數說明: http://www.ispeech.org/api/#voices-standard
+//
+//如果要使用 iSpeech TTS，就拿掉下面這[5行]最左邊的註解(兩個斜線)
+//tts_language = 'ukenglishfemale';  //auenglishfemale , usenglishfemale , chchinesefemale
+//tts_speed = 0;  	//語音的速度 10 ~ -10 (10:最快, 0:一般, -10最慢)
+//tts_pitch = 100; 	//音高 0~200 (100:預設值)
+//tts_base_url = 'https://www.ispeech.org/p/generic/getaudio?action=convert&pitch='+tts_pitch+'&speed='+tts_speed+'&voice='+tts_language+'&text=';
+//tts_cut_last_seconds = 2.54; //english: 2.54 chinese: 5.5 裁掉 iSpeech 最後面的聲音長度(秒為單位)
+
+
+//
+//---------------------------------------------------
+//【題庫設定】
+//---------------------------------------------------
+//
+//計時幾秒
+timer = 180;
+
+//
+//答對一題加幾分
+number_of_points_for_one_question = 10;
+
+//語音播放的速度 (1.0 正常, 0.5 慢速, 2.0 兩倍速)
+audioPlaybackRate = 1.0; 
+
+//
+//每題要拿掉幾個字母當填空
+blanks_total_number = 2;
+
+//
+//欄位分隔符號為兩個井字號(##)
+seperator = '##';
+
+//
+//【題目設定】
+//  一行一題, 
+//  欄位左起
+//    第一欄為英文
+//    第二欄為中文
+//    第三欄為MP3語音檔的路徑(沒有的話，tts_enabled = true 會以第一欄為TTS的朗讀內容)
+//
+questionLines = function(){/*--這一行請勿更改--
+[b]l[a]ck##黑色的##mp3/black.mp3
+[b]lue##藍色的##mp3/blue.mp3
+[r]e[d]##紅色的##mp3/red.mp3
+whi[t]e##白色的##mp3/white.mp3
+[g]ree[n]##綠色的##mp3/green.mp3
+[y]ellow##黃色的##mp3/yellow.mp3
+[u]mbrella##雨傘##mp3/umbrella.mp3
+[u]p##往上##mp3/up.mp3
+[v]an##箱型車##mp3/van.mp3
+[v]est##背心##mp3/vest.mp3
+[w]atch##手錶##mp3/watch.mp3
+[w]indow##窗戶##mp3/window.mp3
+[y]ogurt##優格##mp3/yogurt.mp3
+[y]o-[y]o##溜溜球##mp3/yo-yo.mp3
+[z]ebra##斑馬##mp3/zebra.mp3
+[z]oo##動物園##mp3/zoo.mp3
+-----*/}.toString().replace(/\r/g,"").slice("function(){/*--這一行請勿更改--".length+1,-9);
---
+++ H1U4/data/04.js
@@ -0,0 +1,87 @@
+﻿//=======================================================
+// HTML5 Spelling 題庫設定檔: 使用自定的 MP3 語音檔
+//=======================================================
+//
+//---------------------------------------------------
+//【TTS 語音設定】
+//---------------------------------------------------
+//
+//是否使用 TTS 的語音(使用: true, 不使用: false)
+tts_enabled = false;
+
+//
+//----------------
+//Google TTS 文字轉語音的設定
+//【注意】不能和 iSpeech TTS 同時使用
+//----------------
+//
+//如果要使用 Google TTS，就拿掉下面這[4行]最左邊的註解(兩個斜線)
+//tts_language = 'en';  // en : 英語,   zh_tw : 中文
+//tts_speed = 0.3;  //語音的速度 0 ~ 1 (可用小數)
+//tts_base_url = 'https://translate.google.com/translate_tts?ie=UTF-8&tl='+tts_language+'&client=tw-ob&ttsspeed='+tts_speed+'&q=';
+//tts_cut_last_seconds = 0; //裁掉最後面的聲音長度(秒為單位)
+
+//
+//----------------
+//iSpeech TTS 文字轉語音的設定
+//【注意】不能和 Google TTS 同時使用
+//----------------
+//原始網站參數說明: http://www.ispeech.org/api/#voices-standard
+//
+//如果要使用 iSpeech TTS，就拿掉下面這[5行]最左邊的註解(兩個斜線)
+//tts_language = 'ukenglishfemale';  //auenglishfemale , usenglishfemale , chchinesefemale
+//tts_speed = 0;  	//語音的速度 10 ~ -10 (10:最快, 0:一般, -10最慢)
+//tts_pitch = 100; 	//音高 0~200 (100:預設值)
+//tts_base_url = 'https://www.ispeech.org/p/generic/getaudio?action=convert&pitch='+tts_pitch+'&speed='+tts_speed+'&voice='+tts_language+'&text=';
+//tts_cut_last_seconds = 2.54; //english: 2.54 chinese: 5.5 裁掉 iSpeech 最後面的聲音長度(秒為單位)
+
+
+//
+//---------------------------------------------------
+//【題庫設定】
+//---------------------------------------------------
+//
+//計時幾秒
+timer = 600;
+
+//
+//答對一題加幾分
+number_of_points_for_one_question = 10;
+
+//語音播放的速度 (1.0 正常, 0.5 慢速, 2.0 兩倍速)
+audioPlaybackRate = 1.0; 
+
+//
+//每題要拿掉幾個字母當填空
+blanks_total_number = 2;
+
+//
+//欄位分隔符號為兩個井字號(##)
+seperator = '##';
+
+//
+//【題目設定】
+//  一行一題, 
+//  欄位左起
+//    第一欄為英文
+//    第二欄為中文
+//    第三欄為MP3語音檔的路徑(沒有的話，tts_enabled = true 會以第一欄為TTS的朗讀內容)
+//
+questionLines = function(){/*--這一行請勿更改--
+[black]##黑色的_bkcla##mp3/black.mp3
+[blue]##藍色的_uebl##mp3/blue.mp3
+[red]##紅色的_erd##mp3/red.mp3
+[white]##白色的_hietw##mp3/white.mp3
+[green]##綠色的_gnere##mp3/green.mp3
+[yellow]##黃色的_ywolel##mp3/yellow.mp3
+[umbrella]##雨傘_lmearubl##mp3/umbrella.mp3
+[up]##往上_pu##mp3/up.mp3
+[van]##箱型車_anv##mp3/van.mp3
+[vest]##背心_stve##mp3/vest.mp3
+[watch]##手錶_ahtwc##mp3/watch.mp3
+[window]##窗戶_woindw##mp3/window.mp3
+[yogurt]##優格_tgoruy##mp3/yogurt.mp3
+[yo-yo]##溜溜球_oyoy##mp3/yo-yo.mp3
+[zebra]##斑馬_erbza##mp3/zebra.mp3
+[zoo]##動物園_ooz##mp3/zoo.mp3
+-----*/}.toString().replace(/\r/g,"").slice("function(){/*--這一行請勿更改--".length+1,-9);
