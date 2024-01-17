12b94781be47d4acd5e170f79e0d36e7193b5c44
Silvia Jiang <104666757+byjtw@users.noreply.github.com>
2024-01-17 10:11:38 +0800

    add alphabet

---
+++ alphabet/data/03.js
@@ -0,0 +1,84 @@
+//=======================================================
+// HTML5 Spelling 題庫設定檔: 使用自定的 MP3 語音檔
+//=======================================================
+//
+//---------------------------------------------------
+//【TTS 語音設定】
+//---------------------------------------------------
+//
+//是否使用 TTS 的語音(使用: true, 不使用: false)
+tts_enabled = true;
+
+//
+//----------------
+//Google TTS 文字轉語音的設定
+//【注意】不能和 iSpeech TTS 同時使用
+//----------------
+//
+//如果要使用 Google TTS，就拿掉下面這[4行]最左邊的註解(兩個斜線)
+tts_language = 'en';  // en : 英語,   zh_tw : 中文
+tts_speed = 0.3;  //語音的速度 0 ~ 1 (可用小數)
+tts_base_url = 'https://translate.google.com/translate_tts?ie=UTF-8&tl='+tts_language+'&client=tw-ob&ttsspeed='+tts_speed+'&q=';
+tts_cut_last_seconds = 0; //裁掉最後面的聲音長度(秒為單位)
+
+//
+//----------------
+//iSpeech TTS 文字轉語音的設定
+//【注意】不能和 Google TTS 同時使用
+//----------------
+//原始網站參數說明: http://www.ispeech.org/api/#voices-standard
+//
+//如果要使用 iSpeech TTS，就拿掉下面這[5行]最左邊的註解(兩個斜線)
+//tts_language = 'ukenglishfemale';  //auenglishfemale , usenglishfemale , chchinesefemale
+//tts_speed = 0;  	//語音的速度 10 ~ -10 (10:最快, 0:一般, -10最慢)
+//tts_pitch = 100; 	//音高 0~200 (100:預設值)
+//tts_base_url = 'https://www.ispeech.org/p/generic/getaudio?action=convert&pitch='+tts_pitch+'&speed='+tts_speed+'&voice='+tts_language+'&text=';
+//tts_cut_last_seconds = 2.54; //english: 2.54 chinese: 5.5 裁掉 iSpeech 最後面的聲音長度(秒為單位)
+
+
+//
+//---------------------------------------------------
+//【題庫設定】
+//---------------------------------------------------
+//
+//計時幾秒
+timer = 300;
+
+//
+//答對一題加幾分
+number_of_points_for_one_question = 10;
+
+//語音播放的速度 (1.0 正常, 0.5 慢速, 2.0 兩倍速)
+audioPlaybackRate = 1.0; 
+
+//
+//每題要拿掉幾個字母當填空
+blanks_total_number = 2;
+
+//
+//欄位分隔符號為兩個井字號(##)
+seperator = '##';
+
+//
+//【題目設定】
+//  一行一題, 
+//  欄位左起
+//    第一欄為英文
+//    第二欄為中文
+//    第三欄為MP3語音檔的路徑(沒有的話，tts_enabled = true 會以第一欄為TTS的朗讀內容)
+//
+questionLines = function(){/*--這一行請勿更改--
+[Aa]##提示Aa##mp3/A.mp3
+[Bb]##提示Bb##mp3/B.mp3
+[Cc]##提示Cc##mp3/C.mp3
+[Dd]##提示Dd##mp3/D.mp3
+[Ee]##提示Ee##mp3/E.mp3
+[Ff]##提示Ff##mp3/F.mp3
+[Gg]##提示Gg##mp3/G.mp3
+[Hh]##提示Hh##mp3/H.mp3
+[Ii]##提示Ii##mp3/I.mp3
+[Jj]##提示Jj##mp3/J.mp3
+[Kk]##提示Kk##mp3/K.mp3
+[Ll]##提示Ll##mp3/L.mp3
+[Mm]##提示Mm##mp3/M.mp3
+-----*/}.toString().replace(/\r/g,"").slice("function(){/*--這一行請勿更改--".length+1,-9);
