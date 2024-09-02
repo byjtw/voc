4fa433b48033f6ea5cd228aa1bdd0b642dada1f8
Silvia Jiang <104666757+byjtw@users.noreply.github.com>
2024-09-02 13:24:30 +0800

    H7U1_U2_added

---
+++ H7U1/data/04.js
@@ -0,0 +1,95 @@
+//=======================================================
+// HTML5 Spelling 題庫設定檔: 使用自定的 MP3 語音檔
+//=======================================================
+//
+//【測試區塊】
+//-------------------------------
+//用來偵測換行字元用的測試區塊
+//請勿更動
+//並且放在題庫設定區塊之前
+//-------------------------------
+//測試區塊開始
+CR_LF_test = function(){/*--這一行請勿更改--
+CR_LF testing block
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
+CR_LF_First_Pos = CR_LF_test.indexOf('_LF')-'CR_LF'.indexOf('_LF');
+//測試區塊結束
+//-------------------------------
+//
+
+//---------------------------------------------------
+//【TTS 語音設定】
+//---------------------------------------------------
+//
+//是否使用 TTS 的語音(使用: true, 不使用: false)
+tts_enabled = true;
+
+//
+//----------------
+//Google TTS 文字轉語音的設定
+//【注意】不能和 iSpeech TTS 同時使用
+//----------------
+//
+//如果要使用 Google TTS，就拿掉下面這[4行]最左邊的註解(兩個斜線)
+tts_language = 'en';  // en : 英語,   zh_tw : 中文
+tts_speed = 0.3;  //語音的速度 0 ~ 1 (可用小數)
+tts_base_url = 'https://translate.google.com/translate_tts?ie=UTF-8&tl='+tts_language+'&client=tw-ob&ttsspeed='+tts_speed+'&q=';
+tts_cut_last_seconds = 0; //裁掉最後面的聲音長度(秒為單位)
+
+//
+//----------------
+//iSpeech TTS 文字轉語音的設定
+//【注意】不能和 Google TTS 同時使用
+//----------------
+//原始網站參數說明: http://www.ispeech.org/api/#voices-standard
+//
+//如果要使用 iSpeech TTS，就拿掉下面這[5行]最左邊的註解(兩個斜線)
+//tts_language = 'ukenglishfemale';  //auenglishfemale , usenglishfemale , chchinesefemale
+//tts_speed = 0;  	//語音的速度 10 ~ -10 (10:最快, 0:一般, -10最慢)
+//tts_pitch = 100; 	//音高 0~200 (100:預設值)
+//tts_base_url = 'https://www.ispeech.org/p/generic/getaudio?action=convert&pitch='+tts_pitch+'&speed='+tts_speed+'&voice='+tts_language+'&text=';
+//tts_cut_last_seconds = 2.54; //english: 2.54 chinese: 5.5 裁掉 iSpeech 最後面的聲音長度(秒為單位)
+
+
+//
+//---------------------------------------------------
+//【題庫設定】
+//---------------------------------------------------
+//
+//計時幾秒
+timer = 600;
+
+//
+//答對一題加幾分
+number_of_points_for_one_question = 10;
+
+//
+//每題要拿掉幾個字母當填空
+blanks_total_number = 10;
+
+//
+//欄位分隔符號為兩個井字號(##)
+seperator = '##';
+
+//
+//【題目設定】
+//  一行一題, 
+//  欄位左起
+//    第一欄為英文
+//    第二欄為中文
+//    第三欄為MP3語音檔的路徑(沒有的話，tts_enabled = true 會以第一欄為TTS的朗讀內容)
+//
+questionLines = function(){/*--這一行請勿更改--
+[Australia]##澳洲aiArluast##mp3/Australia.mp3
+[Japan]##日本aanpJ##mp3/Japan.mp3
+[Taiwan]##台灣wianTa##mp3/Taiwan.mp3
+[the] [UK]##英國eht KU##mp3/the UK.mp3
+[the] [USA]##美國eht SAU##mp3/the USA.mp3
+[Spain]##西班牙pinaS##mp3/Spain.mp3
+[Singapore]##新加坡eSraigpno##mp3/Singapore.mp3
+[India]##印度dniaI##mp3/India.mp3
+[where]##哪裡whree##mp3/where.mp3
+[from]##從rmfo##mp3/from.mp3
+[country]##國家yorncut##mp3/country.mp3
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2+(typeof(CR_LF_First_Pos)!='undefined' ? CR_LF_First_Pos:0),-9);
+
