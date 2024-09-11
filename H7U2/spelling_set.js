0ec4e9ad26cbfe52de3a1c5be743e389741c656b
Silvia Jiang <104666757+byjtw@users.noreply.github.com>
2024-09-11 18:25:48 +0800

    Update spelling_set.js
    Update H7U2 set

--- H7U2/spelling_set.js
+++ H7U2/spelling_set.js
@@ -19,7 +19,7 @@ CR_LF_First_Pos = CR_LF_test.indexOf('_LF')-'CR_LF'.indexOf('_LF');
 
 //
 //上方標題
-title = 'H7U1單字高手';
+title = 'H7U2單字高手';
 
 //
 //虛擬鍵盤是否使用數字(使用: true, 不使用: false)
@@ -39,10 +39,10 @@ datafolder = 'data';
 //  以半形逗號為【選項文字】與【題庫檔名】兩欄的分欄符號
 //
 menuItemLines = function(){/*--這一行請勿更改--
-單字高手:H7U1單字抄寫,03.js
-單字高手:H7U1單字依發音規則,01.js
-單字高手:H7U1單字需記憶,02.js
-單字高手:H7U1單字亂碼,04.js
-單字高手:H7U1單字挑戰,05.js
+單字高手:H7U2單字抄寫,03.js
+單字高手:H7U2單字依發音規則,01.js
+單字高手:H7U2單字需記憶,02.js
+單字高手:H7U2單字亂碼,04.js
+單字高手:H7U2單字挑戰,05.js
 -----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2+(typeof(CR_LF_First_Pos)!='undefined' ? CR_LF_First_Pos:0),-9);
 
