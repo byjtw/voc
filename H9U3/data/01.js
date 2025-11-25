7d50064373456bc030dc4eff2a9cdcefa580770e
Silvia Jiang <104666757+byjtw@users.noreply.github.com>
2025-11-25 09:40:58 +0800

    H9U2_U4

---
+++ H9U3/data/01.js
@@ -0,0 +1,98 @@
+//=======================================================
+// HTML5 Spelling 題庫設定檔: 使用自定的 MP3 語音檔
+//=======================================================
+//
+//【測試區塊】
+//-------------------------------
+//用來偵測換行字元用的測試區塊
+//請勿更動
+//並且放在題庫設定區塊之前
+//-------------------------------
+//測試區塊開始
+CR_LF_test = function(){/*--這一行請勿更改--
+CR_LF testing block
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
+CR_LF_First_Pos = CR_LF_test.indexOf('_LF')-'CR_LF'.indexOf('_LF');
+//測試區塊結束
+//-------------------------------
+//
+
+//---------------------------------------------------
+//【TTS 語音設定】
+//---------------------------------------------------
+//
+//是否使用 TTS 的語音(使用: true, 不使用: false)
+tts_enabled = true;
+
+//
+//----------------
+//Google TTS 文字轉語音的設定
+//【注意】不能和 iSpeech TTS 同時使用
+//----------------
+//
+//如果要使用 Google TTS，就拿掉下面這[4行]最左邊的註解(兩個斜線)
+tts_language = 'en';  // en : 英語,   zh_tw : 中文
+tts_speed = 0.3;  //語音的速度 0 ~ 1 (可用小數)
+tts_base_url = 'https://translate.google.com/translate_tts?ie=UTF-8&tl='+tts_language+'&client=tw-ob&ttsspeed='+tts_speed+'&q=';
+tts_cut_last_seconds = 0; //裁掉最後面的聲音長度(秒為單位)
+
+//
+//----------------
+//iSpeech TTS 文字轉語音的設定
+//【注意】不能和 Google TTS 同時使用
+//----------------
+//原始網站參數說明: http://www.ispeech.org/api/#voices-standard
+//
+//如果要使用 iSpeech TTS，就拿掉下面這[5行]最左邊的註解(兩個斜線)
+//tts_language = 'ukenglishfemale';  //auenglishfemale , usenglishfemale , chchinesefemale
+//tts_speed = 0;  	//語音的速度 10 ~ -10 (10:最快, 0:一般, -10最慢)
+//tts_pitch = 100; 	//音高 0~200 (100:預設值)
+//tts_base_url = 'https://www.ispeech.org/p/generic/getaudio?action=convert&pitch='+tts_pitch+'&speed='+tts_speed+'&voice='+tts_language+'&text=';
+//tts_cut_last_seconds = 2.54; //english: 2.54 chinese: 5.5 裁掉 iSpeech 最後面的聲音長度(秒為單位)
+
+
+//
+//---------------------------------------------------
+//【題庫設定】
+//---------------------------------------------------
+//
+//計時幾秒
+timer = 600;
+
+//
+//答對一題加幾分
+number_of_points_for_one_question = 10;
+
+//
+//每題要拿掉幾個字母當填空
+blanks_total_number = 10;
+
+//
+//欄位分隔符號為兩個井字號(##)
+seperator = '##';
+
+//
+//【題目設定】
+//  一行一題, 
+//  欄位左起
+//    第一欄為英文
+//    第二欄為中文
+//    第三欄為MP3語音檔的路徑(沒有的話，tts_enabled = true 會以第一欄為TTS的朗讀內容)
+//
+questionLines = function(){/*--這一行請勿更改--
+a [bag] of##一袋a bag of##mp3/a bag of.mp3
+a [bottle] of##一瓶a bottle of##mp3/a bottle of.mp3
+a [bowl] of##一碗a bowl of##mp3/a bowl of.mp3
+a [box] of##一盒a box of##mp3/a box of.mp3
+a [cup] of##一杯a cup of##mp3/a cup of.mp3
+a [glass] of##一玻璃杯a glass of##mp3/a glass of.mp3
+a [piece] of##一塊a piece of##mp3/a piece of.mp3
+a [plate] of##一盤a plate of##mp3/a plate of.mp3
+[climb]##爬climb##mp3/climb.mp3
+[comb]##梳子comb##mp3/comb.mp3
+[knife]##刀knife##mp3/knife.mp3
+[knit]##針織knit##mp3/knit.mp3
+[knock]##敲knock##mp3/knock.mp3
+[lamb]##羊肉lamb##mp3/lamb.mp3
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2+(typeof(CR_LF_First_Pos)!='undefined' ? CR_LF_First_Pos:0),-9);
+
---
+++ H9U4/data/01.js
@@ -0,0 +1,102 @@
+//=======================================================
+// HTML5 Spelling 題庫設定檔: 使用自定的 MP3 語音檔
+//=======================================================
+//
+//【測試區塊】
+//-------------------------------
+//用來偵測換行字元用的測試區塊
+//請勿更動
+//並且放在題庫設定區塊之前
+//-------------------------------
+//測試區塊開始
+CR_LF_test = function(){/*--這一行請勿更改--
+CR_LF testing block
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
+CR_LF_First_Pos = CR_LF_test.indexOf('_LF')-'CR_LF'.indexOf('_LF');
+//測試區塊結束
+//-------------------------------
+//
+
+//---------------------------------------------------
+//【TTS 語音設定】
+//---------------------------------------------------
+//
+//是否使用 TTS 的語音(使用: true, 不使用: false)
+tts_enabled = true;
+
+//
+//----------------
+//Google TTS 文字轉語音的設定
+//【注意】不能和 iSpeech TTS 同時使用
+//----------------
+//
+//如果要使用 Google TTS，就拿掉下面這[4行]最左邊的註解(兩個斜線)
+tts_language = 'en';  // en : 英語,   zh_tw : 中文
+tts_speed = 0.3;  //語音的速度 0 ~ 1 (可用小數)
+tts_base_url = 'https://translate.google.com/translate_tts?ie=UTF-8&tl='+tts_language+'&client=tw-ob&ttsspeed='+tts_speed+'&q=';
+tts_cut_last_seconds = 0; //裁掉最後面的聲音長度(秒為單位)
+
+//
+//----------------
+//iSpeech TTS 文字轉語音的設定
+//【注意】不能和 Google TTS 同時使用
+//----------------
+//原始網站參數說明: http://www.ispeech.org/api/#voices-standard
+//
+//如果要使用 iSpeech TTS，就拿掉下面這[5行]最左邊的註解(兩個斜線)
+//tts_language = 'ukenglishfemale';  //auenglishfemale , usenglishfemale , chchinesefemale
+//tts_speed = 0;  	//語音的速度 10 ~ -10 (10:最快, 0:一般, -10最慢)
+//tts_pitch = 100; 	//音高 0~200 (100:預設值)
+//tts_base_url = 'https://www.ispeech.org/p/generic/getaudio?action=convert&pitch='+tts_pitch+'&speed='+tts_speed+'&voice='+tts_language+'&text=';
+//tts_cut_last_seconds = 2.54; //english: 2.54 chinese: 5.5 裁掉 iSpeech 最後面的聲音長度(秒為單位)
+
+
+//
+//---------------------------------------------------
+//【題庫設定】
+//---------------------------------------------------
+//
+//計時幾秒
+timer = 600;
+
+//
+//答對一題加幾分
+number_of_points_for_one_question = 10;
+
+//
+//每題要拿掉幾個字母當填空
+blanks_total_number = 10;
+
+//
+//欄位分隔符號為兩個井字號(##)
+seperator = '##';
+
+//
+//【題目設定】
+//  一行一題, 
+//  欄位左起
+//    第一欄為英文
+//    第二欄為中文
+//    第三欄為MP3語音檔的路徑(沒有的話，tts_enabled = true 會以第一欄為TTS的朗讀內容)
+//
+questionLines = function(){/*--這一行請勿更改--
+[April]##四月April##mp3/April.mp3
+[August]##八月August##mp3/August.mp3
+[December]##十二月December##mp3/December.mp3
+[February]##二月February##mp3/February.mp3
+[January]##一月January##mp3/January.mp3
+[July]##七月July##mp3/July.mp3
+[June]##六月June##mp3/June.mp3
+[knight]##騎士knight##mp3/knight.mp3
+[light]##光light##mp3/light.mp3
+[March]##行進March##mp3/March.mp3
+[May]##五月May##mp3/May.mp3
+[November]##十一月November##mp3/November.mp3
+[October]##十月October##mp3/October.mp3
+[right]##正確的right##mp3/right.mp3
+[September]##九月September##mp3/September.mp3
+[wrap]##裹wrap##mp3/wrap.mp3
+[write]##寫write##mp3/write.mp3
+[wrong]##錯誤的wrong##mp3/wrong.mp3
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2+(typeof(CR_LF_First_Pos)!='undefined' ? CR_LF_First_Pos:0),-9);
+
